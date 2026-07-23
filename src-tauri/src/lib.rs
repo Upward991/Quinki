a9beb1285c7f75031611750c3158f65d7a709367
Upward <Upward991@users.noreply.github.com>
2026-07-23 21:01:05 +0200

    Always show titlebar - hide only in fullscreen

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -53,7 +53,7 @@ pub fn run() {
         
         match cmd.spawn() {
           Ok((mut rx, _child)) => {
-            log::info!("Sidecar start script launched — rebuild v109 BUILDRS");
+            log::info!("Sidecar start script launched — rebuild v110 BUILDRS");
             std::thread::spawn(move || {
               while let Some(_event) = rx.blocking_recv() {}
             });
